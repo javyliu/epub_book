9cb4613c93aafd3f3f75cdcf569a0977101de448
qmliu <qmliu@pearlinpalm.com>
2022-07-09 22:20:00 +0800

    fix judge_encoding bug

--- lib/epub_book/book.rb
+++ lib/epub_book/book.rb
@@ -226,7 +226,7 @@ module EpubBook
       EpubBook.logger.info str.encoding
       #/<meta.*?charset\s*=[\s\"\']?utf-8/i =~ str ? str : str.force_encoding('gbk').encode!('utf-8',invalid: :replace, undef: :replace)
 
-      str.scrub('')! unless str.valid_encoding?
+      str.scrub!('') unless str.valid_encoding?
       str.encode!('utf-8') if str.encoding.name != 'UTF-8'
 
       EpubBook.logger.info "-------encode 后 #{str.encoding}"
